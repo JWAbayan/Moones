212483fc0c2a01a7ac4e98ff955a40d53874dc82
johnwpabayan@gmail.com <johnwpabayan@gmail.com>
2025-05-09 00:34:12 +0800

    Implemented the base layout of the Homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
+import HappyEmoji from "/img/emoji_happy.webp"
+
 export default function Home({}){
     return(
-        <div className="flex flex-col w-screen font-display p-6" >
+        <div className="flex flex-col  w-screen h-screen font-display p-6" >
             <header className="flex">
                 <h1 className=" font-bold uppercase" >Moones</h1>
             </header>
-            <main>
+            <main className="flex flex-col items-center">
+                <div className="flex flex-col justify-center items-center mt-20">
+                    <img className="h-55 w-55" src={HappyEmoji} alt="happy-emoji" />
+                </div>
                 <div className="flex flex-col justify-center items-center text-center">
-                    <h1 className="text-4xl font-black uppercase mt-10">
+                    <h1 className="text-4xl font-black uppercase mt-15">
                         The <span className="text-yellow "> music player </span> 
                         <br/> 
                         that matches 
@@ -14,18 +19,16 @@ export default function Home({}){
                         your 
                         <span className="text-yellow"> mood </span>
                     </h1>
-                    <h2>
-                        Discover and listen to music 
+                    <h2 className="mt-8">
+                        Discover and listen to indie music 
                         <br/>
                         that gets your every mood
                     </h2>
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                    <img src="" alt="" />
-                    <h1>
-                        Tap to discover
-                    </h1>
-                </div>
+
+                <button className="bg-yellow p-3 rounded-xl mt-15 font-bold shadow-xl hover:scale-105 transition-all ease-in">
+                    Start setting the mood
+                </button>
             </main>
         </div>
     )
